00b6215c0a9a85ef2466669ba9570325a9f64265
Manoel Rocha <manoelmelo367@gmail.com>
2024-04-23 09:32:29 -0300

    fix: config webhook post config 8

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -693,6 +693,7 @@ export class OrdersService {
     const pix = body.pix;
 
     console.log(pix)
+    console.log(req.ip)
     if (req.ip === "34.193.116.226") {
       pix.forEach(async (pix) => {
         const order = await this.orderModel.findOne({ "payment_intent.payment_intent_info.txid": pix.txid }).lean().exec();
